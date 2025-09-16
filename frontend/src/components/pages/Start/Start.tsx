d42fcdd3418e7a0645f78ef36f71acbecaa1d6ff
biela000 <86307737+biela000@users.noreply.github.com>
2025-09-16 18:38:38 +0200

    add a physical timer for password, modify the previous one by making it cleaner

--- frontend/src/components/pages/Start/Start.tsx
+++ frontend/src/components/pages/Start/Start.tsx
@@ -4,17 +4,17 @@ import KeyButton from '../../ui/KeyButton/KeyButton'
 import Input from '../../ui/Input/Input'
 import { CheckIfUserExists } from '../../../../wailsjs/go/main/App'
 
+const PASSWORD_TIMER = 5
+
 export default function Start() {
   const [password, setPassword] = useState('')
   const [inputVisible, setInputVisible] = useState(false)
   const [userExists, setUserExists] = useState(true)
+  const [buttonTimerAmt, setButtonTimerAmt] = useState(0)
 
   const passwordInput = useRef<HTMLInputElement>(null)
 
   const handleKeyButtonClick = () => {
-    if (inputVisible) {
-    }
-
     setInputVisible(true)
   }
 
@@ -29,24 +29,38 @@ export default function Start() {
   useEffect(() => {
     if (inputVisible) {
       passwordInput.current?.focus()
-
-      setTimeout(() => {
-        setInputVisible(false)
-      }, 5000)
+      setButtonTimerAmt(PASSWORD_TIMER)
     } else {
       setPassword('')
     }
-  }, [inputVisible, setInputVisible, setPassword])
+  }, [inputVisible, setInputVisible, setPassword, PASSWORD_TIMER])
+
+  useEffect(() => {
+    let timeout: number;
+
+    if (buttonTimerAmt === 0) {
+      setInputVisible(false)
+    } else {
+      timeout = setTimeout(() => {
+        setButtonTimerAmt(prevButtonTimerAmt => prevButtonTimerAmt - 1)
+      }, 1000)
+    }
+
+    return () => clearTimeout(timeout)
+  }, [buttonTimerAmt, setInputVisible, setButtonTimerAmt])
 
   return (
     <div className={classes.wrapper}>
-      {(!userExists &&
+      {!userExists &&
         <p className={classes.reminder}>
           if this is your first time, remember the password you input
         </p>
-      )}
-      <KeyButton onClick={handleKeyButtonClick}>
+      }
+      <KeyButton className={classes.button} onClick={handleKeyButtonClick}>
         get in
+        <span className={`${classes.timer} ${buttonTimerAmt > 0 && classes.visible}`}>
+          {buttonTimerAmt}
+        </span>
       </KeyButton>
       <Input
         type="password"
--- frontend/src/components/ui/KeyButton/KeyButton.tsx
+++ frontend/src/components/ui/KeyButton/KeyButton.tsx
@@ -3,12 +3,13 @@ import classes from './KeyButton.module.css'
 export interface KeyButtonProps {
   children?: React.ReactNode
   onClick?: React.MouseEventHandler<HTMLButtonElement>
+  className?: string
 }
 
-export default function KeyButton({ onClick, children }: KeyButtonProps) {
+export default function KeyButton({ className, onClick, children }: KeyButtonProps) {
   return (
     <div className={classes.wrapper}>
-      <button className={classes.button} onClick={onClick}>
+      <button className={`${classes.button} ${className}`} onClick={onClick}>
         {children}
       </button>
     </div>
