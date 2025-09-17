65b5b2b31a34d9a1ba6eac432851d9b85b674cb6
Jakub Biela <jak.biela000@gmail.com>
2025-09-17 07:04:25 +0200

    add ners list to the ners page

--- frontend/src/components/pages/Ners/Ners.tsx
+++ frontend/src/components/pages/Ners/Ners.tsx
@@ -1,9 +1,11 @@
 import classes from './Ners.module.css'
+import NersList from './NersList/NersList'
 
 export default function Ners() {
   return (
     <div className={classes.wrapper}>
-      <h1>Ners Page</h1>
+      <NersList />
+      <div>cmd</div>
     </div>
   )
 }
---
+++ frontend/src/components/pages/Ners/NersList/NersList.tsx
@@ -0,0 +1,13 @@
+import classes from './NersList.module.css'
+
+export default function NersList() {
+  return (
+    <ul className={classes.list}>
+      <li>Ner 1</li>
+      <li>Ner 2</li>
+      <li>Ner 3</li>
+      <li>Ner 4</li>
+      <li>Ner 5</li>
+    </ul>
+  )
+}
